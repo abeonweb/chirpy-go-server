7e891c7b1e39e3cad28ef64d3aa20de579c5e831
abeonweb <abe@abecodes.com>
2026-04-13 08:53:48 +0100

    server created and initial upload

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{}
+	server.Addr = ":8080"
+	server.Handler = mux
+
+	server.ListenAndServe()
+}
